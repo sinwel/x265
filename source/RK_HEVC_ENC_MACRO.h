830513b5521f8702a46598bffc4550655b461d4e
zxy <zhou2090@126.com>
2014-05-29 11:18:46 +0800

    test intra corrected!

--- source/RK_HEVC_ENC_MACRO.h
+++ source/RK_HEVC_ENC_MACRO.h
@@ -1,8 +1,8 @@
 #ifndef RK_HEVC_ENC_MACRO
 #define RK_HEVC_ENC_MACRO
 
-#define RK_FIX_POINT_REPLACE_DOUBLE_POINT          0
-#define RK_INTRA_RDO_ONLY_ONCE                     0
+#define RK_FIX_POINT_REPLACE_DOUBLE_POINT          1
+#define RK_INTRA_RDO_ONLY_ONCE                     1
 #define RK_INTER_METEST                            1
 #define RK_INTER_CALC_RDO_TWICE                    1
 
